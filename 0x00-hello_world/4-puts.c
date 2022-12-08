820f4f182b6b7022fa2450e6116395e1ad5479dc
Sanusi Abdulqudus <qudussanusi2021@gmail.com>
2022-12-08 20:54:35 +0100

    fix puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(str);
+	return (0);
 }
