b4394c178c56d38a199b1f0e811a1954dacc86b1
Sanusi Abdulqudus <qudussanusi2021@gmail.com>
2022-12-10 00:02:27 +0100

    hh

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,15 +4,22 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 	int j = 0;
 
 	while (j <= 9)
 	{
-	putchar(j + '0');
-      	putchar(',');
-	putchar(' ');
-	j++;
+		putchar(j + '0');
+		if (j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}else
+		{
+			putchar(' ');
+		}
+		j++;
 	}
 	return (0);
 }
