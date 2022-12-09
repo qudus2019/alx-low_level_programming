d54e14a5f51808e99192e910f985b6b6c3c10335
Sanusi Abdulqudus <qudussanusi2021@gmail.com>
2022-12-09 19:43:26 +0100

    3-print_alphabets.cii

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,17 @@ int main(void)
 	char ch = 'a';
 	
 	while (ch <= 'z')
+	{
 		putchar(ch);
 		ch++;
+	}
 	ch = 'A';
 
 	while (ch <= 'Z')
+	{
 		putchar(ch);
 		ch++;
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 };
