330e4e01a4aa9f9b23505354ee50053006be320e
Sanusi Abdulqudus <qudussanusi2021@gmail.com>
2022-12-10 00:29:00 +0100

    d

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
+ #include <stdio.h>
 /**
  * main - entry point
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-
 int main(void) {
-  for (int i = 0; i <= 9; i++)
+  int i = 0;
+  while (i <= 9)
   {
-    for (int j = i + 1; j <= 9; j++)
+    int j = i + 1;
+    while (j <= 9)
     {
       putchar(i + '0');
       putchar(',');
       putchar(' ');
       putchar(j + '0');
+      j++;
     }
+    i++;
   }
 
   return (0);
 }
-
