6eb571ccc0b7ef85d1a95fede44eddda3ec4e9dc
Sanusi Abdulqudus <qudussanusi2021@gmail.com>
2022-12-10 00:33:11 +0100

    foz

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,20 +4,20 @@
  * Return: Always 0
  */
 int main(void) {
-  int i = 0;
-  while (i <= 9)
-  {
-    int j = i + 1;
-    while (j <= 9)
-    {
-      putchar(i + '0');
-      putchar(',');
-      putchar(' ');
-      putchar(j + '0');
-      j++;
-    }
-    i++;
-  }
+	int i = 0;
+	while (i <= 9)
+	{
+		int j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+	i++;
+	}
 
-  return (0);
+	return (0);
 }
