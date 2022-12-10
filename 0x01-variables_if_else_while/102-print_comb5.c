dba6a34eccd773b17b47efa7bc4c4ca41fdd2135
Sanusi Abdulqudus <qudussanusi2021@gmail.com>
2022-12-10 07:55:55 +0100

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int j = 0;
+
+		while (j <= 9)
+		{
+			int k = 0;
+
+			while (k <= 9)
+			{
+				int l = 1;
+
+				while (l <= 9)
+				{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(' ');
+			putchar(k + '0');
+			putchar(l + '0');
+			putchar(',');
+			putchar(' ');
+			l++;
+				}
+			k++;
+			}
+			j++;
+		}
+	i++;
+	}
+
+	return (0);
+}
