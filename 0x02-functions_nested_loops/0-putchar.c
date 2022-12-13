c420bc6ad3c44cd72ac6c3f23892f8007639cd44
Sanusi Abdulqudus <qudussanusi2021@gmail.com>
2022-12-13 14:43:25 +0100

    0i

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar");
+	myFunc("_putchar");
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+/**
+ * myFunc - function that prints the specified string
+ * Return_ : Always 0 (Success)
+ * @st+/(r: string to be printed
+ *///
+int myFun(char str[])
+{
+	printf("%s", str);
+	return (0);
+}
