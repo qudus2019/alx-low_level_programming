5a07730bd654ab4cf8b7415405af9d1d6b04df65
Sanusi Abdulqudus <qudussanusi2021@gmail.com>
2022-12-14 15:57:03 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+1#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
